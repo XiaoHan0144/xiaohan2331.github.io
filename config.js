4cddde5e0548ba1b4ffa912eb9aa2108bcf9e69f
Xiaohan2331 <xiaohan3718@qq.com>
2024-05-28 22:08:00 +0800

    first commit

---
+++ config.js
@@ -0,0 +1,33 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'Xiaohan的站点运行情况',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur2563529-41a125732fd60618543eafa5',
+  ],
+
+  // 日志天数
+  CountDays: 50,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Homepage',
+      url: 'https://status.org.cn/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/yb/uptime-status'
+    },
+    {
+      text: 'Blog',
+      url: 'https://abo.xyz/'
+    },
+  ],
+};
